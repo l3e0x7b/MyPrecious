cde92ed851561c98a1842d9d9d2459bede6634b8
L3e0x7b <lyq0x7b@foxmail.com>
2019-07-01 16:55:03 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# Install or upgrade v2ray.
+# Install V2Ray, and run upgrade weekly.
 
-bash <(curl -L -s https://install.direct/go.sh)
+echo "bash <(curl -L -s https://install.direct/go.sh)" > /opt/v2ray_inst.sh
+echo "0 0 * * 5 root /bin/bash /opt/v2ray_inst.sh &> /var/log/v2ray/install.log" >> /etc/crontab
+
+systemctl restart crond &> /dev/null
