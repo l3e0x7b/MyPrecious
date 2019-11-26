c11abcee1c98d4e184723f0d0de649684198c486
l3e0x7b <lyq0x7b@foxmail.com>
2019-11-26 14:29:18 +0800

    Update Shell scripts/v2ray.sh

--- Shell scripts/v2ray.sh
+++ Shell scripts/v2ray.sh
@@ -4,4 +4,6 @@
 echo "bash <(curl -L -s https://install.direct/go.sh)" > /opt/v2ray_inst.sh
 echo "0 0 * * 5 root /bin/bash /opt/v2ray_inst.sh &> /var/log/v2ray/install.log" >> /etc/crontab
 
+/bin/bash /opt/v2ray_inst.sh
+
 systemctl restart crond &> /dev/null
