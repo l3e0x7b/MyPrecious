2f447e28d3f3ec43c8fea230aec182a91b09bc46
l3e0x7b <lyq0x7b@foxmail.com>
2019-12-13 14:20:24 +0800

    Update Shell scripts/docker_install.sh

--- Shell scripts/docker_install.sh
+++ Shell scripts/docker_install.sh
@@ -4,38 +4,49 @@ curl -fsSL get.docker.com -o get-docker.sh
 if [[ $? -eq 0 ]]; then
 	sh get-docker.sh --mirror Aliyun
 
-	systemctl enable docker
-	systemctl start docker
+	if [[ $? -eq 0 ]]; then
+		systemctl enable docker
+		systemctl start docker
 
-	if [[ -f /etc/redhat-release ]]; then
-		docker info | grep "WARNING: bridge-nf-call-iptables is disabled" &> /dev/null
-		if [[ $? -eq 0 ]]; then
-			echo "net.bridge.bridge-nf-call-iptables = 1"
-		fi
-		docker info | grep "WARNING: bridge-nf-call-ip6tables is disabled" &> /dev/null
-		if [[ $? -eq 0 ]]; then
-			echo "net.bridge.bridge-nf-call-ip6tables = 1"
-		fi
-		sysctl -p
-	fi
+		if [[ -f /etc/redhat-release ]]; then
+			sysctl -p
 
-	cat <<-EOF > /etc/docker/daemon.json
+			docker info | grep "WARNING: bridge-nf-call-iptables is disabled" &> /dev/null
+			if [[ $? -eq 0 ]]; then
+				echo "net.bridge.bridge-nf-call-iptables = 1"
+			fi
+			
+			docker info | grep "WARNING: bridge-nf-call-ip6tables is disabled" &> /dev/null
+			if [[ $? -eq 0 ]]; then
+				echo "net.bridge.bridge-nf-call-ip6tables = 1"
+			fi
+			
+			sysctl -p
+		fi
+
+		cat <<-EOF > /etc/docker/daemon.json
 {
-  "registry-mirrors": [
-	"https://dockerhub.azk8s.cn",
-	"https://reg-mirror.qiniu.com"
-  ]
+	"registry-mirrors": [
+		"https://dockerhub.azk8s.cn",
+		"https://reg-mirror.qiniu.com"
+	]
 }
 EOF
 
-	systemctl daemon-reload
-	systemctl restart docker
+		systemctl daemon-reload
+		systemctl restart docker
 
-	# install docker-compose
-	curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
+		rm -f get-docker.sh
 
-	rm -f get-docker.sh
+		# install docker-compose
+		dc_ver=`curl -s https://docs.docker.com/compose/install/ | grep -A1 "The instructions below outline installation" | sed '1d;s/.*<strong>v//;s/<\/strong>.*$//'`
+		curl -L https://github.com/docker/compose/releases/download/${dv_ver}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+		if [[ $? -eq 0 ]]; then
+			chmod +x /usr/local/bin/docker-compose
+		fi
+	else
+		exit 3
+	fi
 else
 	exit 2
 fi
