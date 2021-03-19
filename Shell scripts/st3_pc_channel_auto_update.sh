995c1a367eeda88a1e51a67a735b984f229e9464
l3e0x7b <lyq0x7b@foxmail.com>
2021-03-19 14:48:46 +0800

    Update files

--- Shell scripts/st3_pc_channel_auto_update.sh
+++ Shell scripts/st3_pc_channel_auto_update.sh
@@ -15,7 +15,7 @@ curl -Ssl https://packagecontrol.io/channel_v3.json -o Files/channel_v3.json
 
 git add Files/channel_v3.json
 git commit -m "Update Files/channel_v3.json"
-git push origin master || git push origin main
+git push origin main
 
 if ! grep channel_v3 /etc/crontab &> /dev/null; then
     echo "0 0 * * * root ${script_path}/st3_pc_channel_auto_update.sh &> /dev/null" >> /etc/crontab
