dabe95cccfec9718c82e0ee7bb92b1c097db5cfd
l3e0x7b <lyq0x7b@foxmail.com>
2021-05-24 14:11:31 +0800

    Update

--- ShellScripts/st3_pc_channel_auto_update.sh
+++ ShellScripts/st3_pc_channel_auto_update.sh
@@ -5,7 +5,7 @@
 ## Author: l3e0x7b, <lyq0x7b@foxmail.com>
 ##
 
-script_path='/opt'      # 此脚本所在路径
+script_path='/opt/MyPrecious/ShellScripts'      # 此脚本所在路径
 repo='/opt/MyPrecious'      # GitHub 仓库路径
 
 cd ${repo} || exit
@@ -18,7 +18,7 @@ git commit -m "Update Files/channel_v3.json"
 git push origin main
 
 if ! grep st3_pc_channel_auto_update /etc/crontab &> /dev/null; then
-    echo "0 0 * * * root ${script_path}/st3_pc_channel_auto_update.sh &> /dev/null" >> /etc/crontab
+    echo "0 0 * * * root bash ${script_path}/st3_pc_channel_auto_update.sh &> /dev/null" >> /etc/crontab
 
     systemctl restart cron || systemctl restart crond
 fi
