cac7d53393d1b647d9bd05d2139094905c50cab6
l3e0x7b <lyq0x7b@foxmail.com>
2022-03-02 15:08:13 +0800

    Update

--- ShellScripts/xray_dat_upgrade.sh
+++ ShellScripts/xray_dat_upgrade.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-##
-## Description: Automatically update geo* files for v2ray/xray.
-##
-## Author: l3e0x7b, <lyq0x7b@foxmail.com>
-##
 
-ver=$(curl -s https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest | grep "tag_name" | sed 's/^.*: "//;s/",.*$//')
-dir='/usr/local/share/xray'
+xray_ver=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep "tag_name" | sed 's/^.*: "//;s/",.*$//')
+xray_path='/usr/local/bin'
+dat_ver=$(curl -s https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest | grep "tag_name" | sed 's/^.*: "//;s/",.*$//')
+dat_path='/usr/local/share/xray'
+
+if curl -sL https://github.com/XTLS/Xray-core/releases/download/"${xray_ver}"/Xray-linux-64.zip -o /tmp/Xray-linux-64.zip; then
+        unzip -q -d /tmp /tmp/Xray-linux-64.zip xray
+        mv /tmp/xray ${xray_path}
+        chmod +x ${xray_path}/xray
+        systemctl restart xray.service
+        rm -f /tmp/Xray-linux-64.zip
+fi
 
 for file in geoip.dat geosite.dat; do
-    if curl -sL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/"${ver}"/${file} -o /tmp/${file}; then
-        mv -f /tmp/${file} ${dir}
-
+    if curl -sL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/"${dat_ver}"/${file} -o /tmp/${file}; then
+        mv -f /tmp/${file} ${dat_path}
         systemctl restart xray.service
     fi
 done
