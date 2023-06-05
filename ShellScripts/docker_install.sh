21404b1378a1ce2982f086c2cd4990d521826fe5
l3e0x7b <lyq0x7b@foxmail.com>
2023-06-05 17:37:04 +0800

    Update docker_install.sh

--- ShellScripts/docker_install.sh
+++ ShellScripts/docker_install.sh
@@ -1,73 +1,36 @@
 #!/bin/bash
 ##
-## Description: Docker & Docker Compose installation script for Linux.
+## Description: Docker installation script for Debian.
 ##
 ## Author: l3e0x7b, <lyq0x7b@foxmail.com>
 ##
 
-# Install Docker
-inst_docker () {
-    if curl -sSL get.docker.com -o get-docker.sh; then
-        
-        if sh get-docker.sh --mirror Aliyun; then
-    
-            if [[ -f /etc/redhat-release ]]; then
-                sysctl -p
-    
-                grep '1' /proc/sys/net/bridge/bridge-nf-call-iptables &> /dev/null && echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf
-                grep '1' /proc/sys/net/bridge/bridge-nf-call-ip6tables &> /dev/null && echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.conf
-                
-                sysctl -p
-            elif [[ -f /etc/debian_version ]]; then
-            
-                if ! grep '^GRUB_CMDLINE_LINUX=' /etc/default/grub | grep 'cgroup_enable=memory swapaccount=1' &> /dev/null; then
-                    sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 cgroup_enable=memory swapaccount=1"/' /etc/default/grub
-                    
-                    update-grub
-                fi
-            fi
+apt update && apt -y install apt-transport-https ca-certificates curl gnupg lsb-release
 
-            systemctl enable docker
-            systemctl start docker
+mkdir -p /etc/apt/keyrings
 
-            cat <<-EOF > /etc/docker/daemon.json
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
+apt update && apt -y install docker-ce docker-ce-cli docker-ce-rootless-extras
+
+if docker info; then
+  cat <<EOF > /etc/docker/daemon.json
 {
-    "exec-opts": ["native.cgroupdriver=systemd"],
-    "log-driver": "json-file",
-    "log-opts": {"max-size": "100m"},
-    "storage-driver": "overlay2",
-    "storage-opts": ["overlay2.override_kernel_check=true"],
-    "registry-mirrors": [
-        "https://tuyjisn6.mirror.aliyuncs.com",
-        "https://hub-mirror.c.163.com"
-    ]
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-opts": {"max-size": "100m"},
+  "storage-driver": "overlay2",
+  "registry-mirrors": [
+    "https://tuyjisn6.mirror.aliyuncs.com",
+    "https://docker.mirrors.ustc.edu.cn",
+    "https://hub-mirror.c.163.com"
+  ],
+  "insecure-registries": ["127.0.0.1"]
 }
 EOF
 
-            systemctl daemon-reload
-            systemctl restart docker
-
-            rm -f get-docker.sh
-
-        
-        else
-            echo "Failed to install docker." && exit
-        fi
-    else
-        echo "Failed to download [get-docker.sh]." && exit
-    fi
-}
-
-# Install Docker Compose
-inst_compose () {
-    dc_ver=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep "tag_name" | sed 's/^.*: "//;s/",.*$//')
-
-    if curl -L https://github.com/docker/compose/releases/download/"${dc_ver}"/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose; then
-        chmod +x /usr/local/bin/docker-compose
-    else
-        echo "Failed to install docker-compose." && exit
-    fi
-}
-
-inst_docker
-inst_compose
+  systemctl daemon-reload
+  systemctl restart docker.service
+else
+  echo "Failed to install docker." && exit
+fi
